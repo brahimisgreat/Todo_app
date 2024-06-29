c5e80b9f001ad1e4c25dc5c4d353d7d7f3617a9b
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-06-28 21:14:35 -0400

    backend stated

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.json());
+
+const port = 5000;
