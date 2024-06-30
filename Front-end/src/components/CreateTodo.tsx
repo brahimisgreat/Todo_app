7d33dd1d4151198ddd8a22c6891abb90fad8cca4
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-06-30 15:06:45 -0400

    yes

--- Front-end/src/components/CreateTodo.tsx
+++ Front-end/src/components/CreateTodo.tsx
@@ -1,11 +1,20 @@
 import './CreateTodo.scss'
+import { useState } from 'react'
 
 export const CreateTodo = () => {
+
+ const [todo, setTodo] = useState('')
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+    e.preventDefault()
+    console.log('submitted')
+    console.log(todo)
+  }
   return (
     <div className='createTodo'>
-        <form>
-            <button><div></div></button>
-            <input type='text' placeholder='Create a new todo...'/>
+        <form onSubmit={handleSubmit}>
+            <button type='submit'><div></div></button>
+            <input type='text' placeholder='Create a new todo...' value={todo} onChange={(e)=> {setTodo(e.target.value)}} />
         </form>
 
     </div>
