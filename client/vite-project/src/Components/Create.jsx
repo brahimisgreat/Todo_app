40e190d7624ebc1f0a8aadaad7548071695d06d0
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-07-28 02:17:18 -0400

    fsfd

--- client/vite-project/src/Components/Create.jsx
+++ client/vite-project/src/Components/Create.jsx
@@ -1,29 +1,33 @@
-import {useState} from 'react'
-import axios from 'axios'
-import '../App.scss'
-import circle from '../assets/assets/circle.svg'
+import { useState } from "react";
+import axios from "axios";
+import './Create.scss';
+import circle from "../assets/assets/circle.svg";
 
 export const Create = () => {
+  const [task, setTask] = useState("");
 
-    const [task, setTask] = useState('')
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios.post('http://localhost:3000/create/', {item: task})
-        .then((res) => {
-            console.log(res)
-        }).catch((err) => {
-            console.log(err)
-        })
-    }
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/create/", { item: task })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <div className='create'>
-    
-    <img src={circle} alt='circle'  onClick={handleSubmit}/>
-        <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
-        
+    <div className="create">
+      <img src={circle} alt="circle" onClick={handleSubmit} />
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Create a new todo..."
+        required
+      />
     </div>
-  )
-}
+  );
+};
