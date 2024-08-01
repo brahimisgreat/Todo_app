64d55910c6e71c7d9eb0f2187607fd89914d3cb1
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-07-31 23:46:14 -0400

    fdfdsf

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,26 +1,32 @@
-import {  } from 'react'
-import './App.scss'
-import { Create } from './Components/Create'
-import bg from '../src/assets/assets/bg-mobile-light.jpg'
-import { Header } from './Components/Header'
-import { List } from './Components/List'
-import bgd from './assets/assets/bg-desktop-light.jpg'
+import { useState } from "react";
+import "./App.scss";
+import { Create } from "./Components/Create";
+import bgL from "../src/assets/assets/bg-mobile-light.jpg";
+import { Header } from "./Components/Header";
+import { List } from "./Components/List";
+import bgdL from "./assets/assets/bg-desktop-light.jpg";
+import { themeContext } from "./Components/Themecontext";
+import  bgdD from './assets/assets/bg-desktop-dark.jpg'
+import bgmD from './assets/assets/bg-mobile-dark.jpg'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
     <div className="app">
-      <div className="hero">
-        <img src={bgd} className='bg-desktop' alt="background image" />
-        <img src={bg} className='bg-mobile' alt="background image" />
-      </div>
-      <div className="content">
-      <Header />
-      <Create />
-      <List />
-      </div>
+      <themeContext.Provider value={{ darkMode, setDarkMode }}>
+        <div className="hero">
+          <img src={darkMode? bgdD :  bgdL} className="bg-desktop" alt="background image" />
+          <img src={darkMode? bgmD : bgL} className="bg-mobile" alt="background image" />
+        </div>
+        <div className="content">
+          <Header />
+          <Create />
+          <List />
+        </div>
+      </themeContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/vite-project/src/Components/Header.jsx
+++ client/vite-project/src/Components/Header.jsx
@@ -1,12 +1,24 @@
 import todo from "../assets/assets/todo.svg";
 import moon from "../assets/assets/icon-moon.svg";
 import "./Header.scss";
+import sun from '../assets/assets/icon-sun.svg'
+import { useContext } from "react";
+import { themeContext } from "./Themecontext";
+
 
 export const Header = () => {
+  const {darkMode} = useContext(themeContext);
+  const {setDarkMode} = useContext(themeContext);
+
+console.log(darkMode)
+  function handleClick() {
+    setDarkMode(!darkMode)
+  }
+
   return (
     <header>
       <img className="todo-img" src={todo} alt="todo" />
-      <img  className='moon' src={moon} alt="moon" />
+      {darkMode ? <img className="sun" src={sun} onClick={handleClick} /> :  <img  onClick={handleClick} className='moon' src={moon} alt="moon" /> }
     </header>
   );
 };
